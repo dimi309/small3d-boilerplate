12d1e67a3ae2ea5f06ea8d5f68a1b8b4535b2414
dimi309 <dimi309@users.noreply.github.com>
2024-11-19 00:29:42 +0100

    Adapted to latest small3d.

--- src/Global.hpp
+++ src/Global.hpp
@@ -4,7 +4,7 @@
 #include <small3d/Renderer.hpp>
 
 const std::string resourceDir = "resources";
-const glm::vec4 skyColour(0.5f, 0.5f, 7.0f, 1.0f);
+const small3d::Vec4 skyColour(0.5f, 0.5f, 7.0f, 1.0f);
 extern small3d::Renderer* r;
 const std::string appName = "Game";
 
